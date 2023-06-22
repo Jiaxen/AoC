760f50b159ef37945573b37b58d10e14cef91022
jiaxen <jiaxen.lau@gmail.com>
2023-06-22 22:45:46 +0100

    day 1

---
+++ 2021/python/day_1.py
@@ -0,0 +1,18 @@
+
+def get_data():
+    with open('2021/inputs/day_1') as f:
+        measurements = f.readlines()
+    return tuple(int(i) for i in measurements)
+
+
+def solution_1():
+    measurements = get_data()
+    result = 0
+    for x in range(1, len(measurements)):
+        if measurements[x] > measurements[x-1]:
+            result += 1
+    return result
+
+
+if __name__ == '__main__':
+    print(solution_1())
